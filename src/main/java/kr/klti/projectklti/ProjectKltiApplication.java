6e562e4aab0b663c2b48e264ba97142c0c0da28d
오동현 <kmcpyj0@gmail.com>
2023-08-08 00:05:49 +0900

    첫 커밋

---
+++ src/main/java/kr/klti/projectklti/ProjectKltiApplication.java
@@ -0,0 +1,13 @@
+package kr.klti.projectklti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectKltiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectKltiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/klti/projectklti/ProjectKltiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.klti.projectklti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectKltiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
