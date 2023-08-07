df821a844f3afb2d0ad16fe21e53333bedbf98a3
오동현 <kmcpyj0@gmail.com>
2023-08-08 00:40:39 +0900

    템플릿 추가

---
+++ src/main/java/kr/klti/projectklti/HomeController.java
@@ -0,0 +1,36 @@
+package kr.klti.projectklti;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//테스트용 초기 컨트롤러입니다.
+//이름 html이름을 따서 만들었습니다.
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+    @RequestMapping("/accounts")
+    public String accounts(){
+        return "accounts";
+    }
+    @RequestMapping("/add")
+    public String add_product(){
+        return "add-product";
+    }
+    @RequestMapping("/edit")
+    public String edit(){
+        return "home";
+    }
+    @RequestMapping("/loginview")
+    public String login(){
+        return "login";
+    }
+    @RequestMapping("/products")
+    public String products(){
+        return "products";
+    }
+
+
+}
