344d648d4178d164306bb1a6671cd6d3053231b1
selgi Kang <k921211@hanmail.net>
2023-08-14 23:18:39 +0900

    2023_08_14 유효성검사 & css

--- frontend/src/pages/Join/Join.js
+++ frontend/src/pages/Join/Join.js
@@ -1,28 +1,33 @@
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
 import {useState} from "react";
-
-
+import './join.css';
 
 
 export default function Join() {
 
     const [name, setName] = useState('');
     const [birth, setBirth] = useState('');
-    const [role, setRole] = useState('강사');
     const [gender, setGender] = useState('여');
     const [userEmail, setUserEmail] = useState('');
     const [userId, setUserId] = useState('');
     const [password, setPassword] = useState('');
+    const [passwordCheck, setPasswordCheck] = useState('');
+    const [passwordError, setPasswordError] = useState('');
     const [createReq, setCreateReq] = useState(new Date());
+    const [role, setRole] = useState('');
+    const [changePassword, setChangePassword] = useState('0');
+    const navigate = useNavigate();
 
     const inputName = e => {
         setName(e.target.value);
     };
-   function handleDateChange(e){
-       const formattedDate = e.target.value.replace(/-/g, '');
-       setBirth(formattedDate);
-   }
+
+    function handleDateChange(e) {
+        const formattedDate = e.target.value.replace(/-/g, '');
+        setBirth(formattedDate);
+    }
+
     const inputGender = e => {
         setGender(e.target.value);
     };
@@ -33,31 +38,77 @@ export default function Join() {
         setUserId(e.target.value);
     };
     const inputPassword = e => {
+        const newPassword = e.target.value;
+
+        if (!/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[a-zA-Z\d@$!%*#?&]{3,}$/.test(newPassword)) {
+            setPasswordError('비밀번호 형식이 올바르지 않습니다.');
+        } else {
+            setPasswordError('');
+        }
         setPassword(e.target.value);
     };
-    const inputCreateReq = e =>{
+    const inputCreateReq = e => {
         setCreateReq(new Date())
     };
 
+    const checkPasswordMatch = () => {
+        if (password !== passwordCheck) {
+            setPasswordError('비밀번호가 일치하지 않습니다.');
+        } else {
+            setPasswordError('');
+        }
+    };
 
+    const inputPasswordCheck = e => {
+        setPasswordCheck(e.target.value);
+        if(password === e.target.value){
+            setPasswordError('');
+        }
+        checkPasswordMatch();
+    };
 
+    function handleFormSubmit(e) {
+        e.preventDefault(); // 기본 제출 동작 방지
+        if (!name || !birth || !gender || !userEmail || !userId || !password || !passwordCheck) {
+            alert('입력값을 확인해주세요.');
+            return;
+        }
+        if (password !== passwordCheck) {
+            setPasswordError('비밀번호가 일치하지 않습니다.');
+            return;
+        }
+
+        if (!/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[a-zA-Z\d@$!%*#?&]{3,}$/.test(password)) {
+            setPasswordError('비밀번호 형식이 올바르지 않습니다.');
+            return;
+        }
+
+        if (passwordError) {
+            alert('형식이 올바르지 않거나 일치하지 않습니다.');
+            return;
+        }
+
+        joinSubmit(); // 위의 모든 조건을 통과하면 실제 가입 함수 호출
+    }
 
     function joinSubmit() {
+
         axios.post('http://localhost:8080/api/auth/join', {
             name: name,
             birth: birth,      // 날짜 넣는법 더 알아보기
-            role: role,
             gender: gender,
             userEmail: userEmail,
             userId: userId,
             password: password,
-            createReq:createReq
+            role:role,
+            createReq: createReq,
+            changePassword: changePassword
         })
             .then(response => {
                 console.log('회원가입 완료!');
                 console.log('response', response);
-                if(response.status === 200) {
-                    window.location.replace("/");
+                if (response.status === 200) {
+                    navigate("/");
                 }
             })
             .catch(error => {
@@ -68,28 +119,63 @@ export default function Join() {
 
     return (
         <>
-            <form method="post">
-                <p>이름 : <input type="text" name="name" onChange={inputName}/></p>
-                <p>생년월일 : <input type="date" id="birth" name="birth" onChange={handleDateChange}/></p>
-                <p>
-                    신분 :
-                    <select name="role">
-                        <option value="강사">강사</option>
-                        <option value="학생">학생</option>
-                    </select>
-                </p>
-                <p>
-                    성별 :
-                    <select name="gender" onChange={inputGender}>
-                        <option value="여">여</option>
-                        <option value="남">남</option>
-                    </select>
-                </p>
-                <p>이메일 : <input type="email" name="userEmail" onChange={inputUserEmail}/></p>
-                <p>ID : <input type="text" name="userId" onChange={inputUserId}/></p>
-                <p>패스워드 : <input type="password" name="password" onChange={inputPassword}/></p>
-                            <input type="button" value="가입" onClick={joinSubmit}/>
-            </form>
+            <div id="joinBody">
+                <h3>회원가입</h3>
+                <form method="post" onSubmit={handleFormSubmit}>
+                    <table id="jointable">
+                        <tr>
+                            <td>이름</td>
+                            <td className="join_td_2"><input type="text" name="name" onChange={inputName} placeholder="이름" required/></td>
+                        </tr>
+                        <tr>
+                            <td>생년월일</td>
+                            <td className="join_td_2"><input type="date" id="birth" name="birth" onChange={handleDateChange} required/></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                성별
+                            </td>
+                            <td className="join_td_2">
+                                <select name="gender" onChange={inputGender} required>
+                                    <option value="여">여</option>
+                                    <option value="남">남</option>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>이메일</td>
+                            <td className="join_td_2"><input type="email" name="userEmail" onChange={inputUserEmail} placeholder="이메일" required/></td>
+                        </tr>
+                        <tr>
+                            <td>ID</td>
+                            <td className="join_td_2"><input type="text" name="userId" onChange={inputUserId} placeholder="아이디" required/></td>
+                        </tr>
+                        <tr>
+                            <td>Password</td>
+                            <td className="join_td_2">
+                                <input type="password" name="password" onChange={inputPassword} placeholder="비밀번호" required/>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>PasswordCheck</td>
+                            <td className="join_td_2"><input type="password" name="passwordCheck" onChange={inputPasswordCheck} placeholder="비밀번호 확인" required/></td>
+                        </tr>
+                        <tr>
+                            <td colSpan="2" className="join_password"><small>숫자,영어,특수문자를 포함해 3자리 이상 입력해주세요.</small></td>
+                        </tr>
+                        {passwordError && <tr><td colSpan="2" className="error_td"><small className="error_message">{passwordError}</small></td></tr>}
+                        <tr>
+                            <td colSpan="2" className="join_btn">
+                                <button
+                                    className="join_btn_inner"
+                                >가입
+                                </button>
+                            </td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
         </>
     )
 }
